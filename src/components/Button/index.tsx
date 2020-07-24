4842249efb54317e002c0719c5bf09db557a0d03
Juliano Sirtori <julianosirtori@gmail.com>
2020-07-24 14:22:15 -0300

    Add storybook

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Button: React.FC = ({children}) => {
+  return <button>{children}</button>;
+}
+
+export default Button;
