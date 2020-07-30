9c87b1045f52941b3ed37b8648a1bb2167724df9
Felipe Maccari <felipe@medplus.com.br>
2020-07-30 20:04:05 -0300

    feat(cafeina): add snapshot test to icon

---
+++ src/components/atoms/Icon/Icon.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Icon } from './Icon'
+import { Text } from '../Text'
+
+import { render } from '../../utils/test/render'
+
+describe('must match with the previous snapshot', () => {
+  test('snapshot renders', () => {
+    const component = render(
+      <Icon>
+        <Text>Icon component!</Text>
+      </Icon>
+    )
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
