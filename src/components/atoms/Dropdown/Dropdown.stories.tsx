19b4e04ad67285cc3ee56aa906e5fe673152b073
Juliano Sirtori <julianosirtori@gmail.com>
2020-07-31 09:59:22 -0300

    Add stories opened

--- src/components/atoms/Dropdown/Dropdown.stories.tsx
+++ src/components/atoms/Dropdown/Dropdown.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Dropdown } from './Dropdown'
+import { Flex } from '../Flex'
 
 export default {
   component: Dropdown,
@@ -16,11 +17,35 @@ export const Basic: React.FC = () => {
   }
 
   return (
-    <Dropdown
-      label={label}
-      maxWidth={240}
-      itens={itens}
-      onChange={handleChangeDropdown}
-    />
+    <Flex variant='fullCentralized'>
+      <Dropdown
+        label={label}
+        width={240}
+        itens={itens}
+        onChange={handleChangeDropdown}
+      />
+    </Flex>
+  )
+}
+
+export const Opened: React.FC = () => {
+  const itens = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']
+  const label = 'Dropdown'
+
+  function handleChangeDropdown(item: any) {
+    console.log(item)
+  }
+
+  return (
+    <Flex variant='fullCentralized'>
+      <Dropdown
+        label={label}
+        opened={true}
+        maxWidth={240}
+        width={'100%'}
+        itens={itens}
+        onChange={handleChangeDropdown}
+      />
+    </Flex>
   )
 }
--- src/components/atoms/Dropdown/Dropdown.tsx
+++ src/components/atoms/Dropdown/Dropdown.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { MdArrowDropDown } from 'react-icons/md'
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md'
 import styled, { DefaultTheme } from 'styled-components'
 import {
   size,
@@ -11,11 +11,18 @@ import {
   LayoutProps
 } from 'styled-system'
 
+import { Box } from '../Box'
+
 export interface Props extends SizeProps, MarginProps, LayoutProps {
   label: string
   itens: string[]
+  opened?: boolean
   onChange?: (item: any) => void
 }
+
+const BoxStyled = styled(Box)`
+  position: relative;
+`
 
 const DropdownStyled = styled.div<DefaultTheme>`
   ${size}
@@ -51,19 +58,79 @@ const DropdownStyled = styled.div<DefaultTheme>`
 
 `
 
-export const Dropdown: React.FC<Props> = ({ label, ...props }) => {
+interface ItensStyledProps extends DefaultTheme, LayoutProps {}
+
+const ItensStyled = styled.ul<ItensStyledProps>`
+  ${layout}
+  list-style: none;
+  border: 1px solid ${props => props.theme.colors.mediumGrey};
+  color: ${props => props.theme.colors.almostBlack};
+  border-radius: 4px;
+  background: ${props => props.theme.colors.white};
+  font-size: ${props => props.theme.fontSizes[3]};
+
+  border-top: none;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  position: absolute;
+  width: 100%;
+  top: 38px;
+
+  li {
+    padding: 8px;
+
+    &:hover {
+      cursor: pointer;
+      background: #fdeaf4;
+    }
+  }
+`
+
+export const Dropdown: React.FC<Props> = ({
+  label,
+  opened,
+  itens,
+  maxWidth,
+  onChange = () => {},
+  ...props
+}) => {
+  const [isOpen, setIsOpen] = useState(opened)
+
+  function handleClickDropdown() {
+    setIsOpen(!isOpen)
+  }
+
+  function handleClickItem(item) {
+    setIsOpen(false)
+    onChange(item)
+  }
+
   return (
-    <DropdownStyled {...props}>
-      <span>{label}</span>
-      <button type='button'>
-        <MdArrowDropDown size={24} />
-      </button>
-    </DropdownStyled>
+    <BoxStyled maxWidth={maxWidth} {...props}>
+      <DropdownStyled onClick={handleClickDropdown}>
+        <span>{label}</span>
+        <button type='button'>
+          {isOpen ? <MdArrowDropUp size={24} /> : <MdArrowDropDown size={24} />}
+        </button>
+      </DropdownStyled>
+      {isOpen && (
+        <ItensStyled maxWidth={maxWidth}>
+          {itens.map(item => (
+            <li onClick={() => handleClickItem(item)} key={item}>
+              {item}
+            </li>
+          ))}
+        </ItensStyled>
+      )}
+    </BoxStyled>
   )
 }
 
 Dropdown.propTypes = {
   label: PropTypes.string.isRequired,
   itens: PropTypes.array.isRequired,
+  opened: PropTypes.bool,
+  maxWidth: PropTypes.number,
+  width: PropTypes.number,
   onChange: PropTypes.func
 }
