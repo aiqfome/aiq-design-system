c014a8ea6f0a5e6c43a32143c51e52e91e9ca0db
Juliano Sirtori <julianosirtori@gmail.com>
2020-08-03 13:40:16 -0300

    fix avatar when src prop is empty

--- src/components/atoms/Avatar/Avatar.stories.tsx
+++ src/components/atoms/Avatar/Avatar.stories.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
 
 import { Avatar } from './Avatar'
 
 export default {
   component: Avatar,
-  title: 'atoms/Avatar'
+  title: 'atoms/Avatar',
+  decorators: [withKnobs]
 }
 
-export const basic: React.FC = () => <Avatar alt='avatar' />
+export const basic: React.FC = () => (
+  <Avatar
+    alt={text('Label', 'Avatar')}
+    src={text('src', '')}
+    palette={select(
+      'palette',
+      { Primary: 'primary', Secondary: 'secondary' },
+      'primary'
+    )}
+  />
+)
 
 export const withUrl: React.FC = () => (
   <Avatar
-    src='https://lh3.googleusercontent.com/rALGk_PU3JMf_5NS5FEYScz9zxgjRBNePvMheCnHIO_lrSs089QcwguwqRVaDLWWAQ'
-    alt='avatar'
+    palette={select(
+      'palette',
+      { Primary: 'primary', Secondary: 'secondary' },
+      'primary'
+    )}
+    src={text(
+      'src',
+      'https://lh3.googleusercontent.com/rALGk_PU3JMf_5NS5FEYScz9zxgjRBNePvMheCnHIO_lrSs089QcwguwqRVaDLWWAQ'
+    )}
+    alt={text('alt', 'Avatar')}
   />
 )
--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { Text } from '../Text'
 
 export interface Props {
   src?: string
+  palette?: string
   alt: string
 }
 
@@ -13,11 +16,56 @@ export const AvatarStyled = styled.img<Props>`
   border-radius: 5px;
 `
 
-export const Avatar: React.FC<Props> = ({ src, alt, ...props }) => {
-  return <AvatarStyled src={src} alt={alt} {...props} />
+interface ContainerStyledProps extends DefaultTheme {
+  palette: string
+}
+
+export const ContainerStyled = styled.div<ContainerStyledProps>`
+  height: 36px;
+  width: 36px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${({ palette, theme }) =>
+    palette === 'primary' &&
+    css`
+      background: ${theme.colors.primaryLight};
+    `}
+  ${({ palette, theme }) =>
+    palette === 'secondary' &&
+    css`
+      background: ${theme.colors.secondaryLight};
+    `}
+`
+
+export const Avatar: React.FC<Props> = ({
+  src,
+  alt,
+  palette = 'primary',
+  ...props
+}) => {
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    setName(alt.substring(0, 1).toUpperCase())
+  }, [alt])
+
+  if (src) {
+    return <AvatarStyled src={src} alt={alt} {...props} />
+  }
+  return (
+    <ContainerStyled palette={palette}>
+      <Text fontWeight='bold' color={palette}>
+        {name}
+      </Text>
+    </ContainerStyled>
+  )
 }
 
 Avatar.propTypes = {
   src: PropTypes.string,
+  palette: PropTypes.string,
   alt: PropTypes.string.isRequired
 }
