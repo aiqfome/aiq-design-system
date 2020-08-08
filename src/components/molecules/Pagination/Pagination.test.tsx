a0383507509108ddf07502f55331a6ef12bc3f7f
Juliano Sirtori <julianosirtori@gmail.com>
2020-08-08 11:02:30 -0300

    add test

--- src/components/molecules/Pagination/Pagination.test.tsx
+++ src/components/molecules/Pagination/Pagination.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 
+import { render } from '../../utils/test/render'
 import { Pagination } from './Pagination'
 
 describe('must match with the previous snapshot', () => {
   test('snapshot renders', () => {
-    const component = renderer.create(<Pagination count={10} />)
+    const component = render(<Pagination count={10} />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
