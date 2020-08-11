ddf7c5a31ab1c8f8d1e26db618afd282c4f3cb06
Juliano Sirtori <julianosirtori@gmail.com>
2020-08-11 13:51:13 -0300

    add knobs

--- src/components/atoms/Divider/Divider.stories.tsx
+++ src/components/atoms/Divider/Divider.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { withKnobs, text, number, color } from '@storybook/addon-knobs'
 
 import { Text } from '../Text'
 import { Flex } from '../Flex'
@@ -6,17 +7,18 @@ import { Divider } from './Divider'
 
 export default {
   component: Divider,
-  title: 'atoms/Divider'
+  title: 'atoms/Divider',
+  decorators: [withKnobs as any]
 }
 
 export const Basic: React.FC = () => (
-  <Flex width='100%'>
-    <Divider width='100%' />
+  <Flex width={text('width', '100%')}>
+    <Divider width={text('width', '100%')} />
   </Flex>
 )
 
 export const withChildren: React.FC = () => (
-  <Divider width='100%' color='primary'>
+  <Divider width={text('width', '100%')} color={color('color', 'primary')}>
     <Text paddingRight={10} paddingLeft={10}>
       Aiqfome!
     </Text>
@@ -24,5 +26,9 @@ export const withChildren: React.FC = () => (
 )
 
 export const Height: React.FC = () => (
-  <Divider margin={10} height={5} color='secondary' />
+  <Divider
+    margin={10}
+    height={number('height', 5)}
+    color={color('color', 'secondary')}
+  />
 )
