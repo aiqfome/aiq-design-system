f7952c9ade344551b1d3fdcbbdd40c1a0b584104
Juliano Sirtori <julianosirtori@gmail.com>
2020-08-12 14:52:24 -0300

    add test

--- src/components/molecules/Breadcrumb/Breadcrumb.test.tsx
+++ src/components/molecules/Breadcrumb/Breadcrumb.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 
-import { Breadcrumb } from './Breadcrumb'
+import { Breadcrumb, BreadcrumbItem } from './Breadcrumb'
+import { Link } from '../../atoms/Link'
+
+import { render } from '../../utils/test/render'
 
 describe('must match with the previous snapshot', () => {
   test('snapshot renders', () => {
-    const component = renderer.create(<Breadcrumb />)
+    const component = render(
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <Link marginLeft='5px' href='#'>
+            início
+          </Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Link href='#'>relatório</Link>
+        </BreadcrumbItem>
+        <BreadcrumbItem>
+          <Link href='#'>pedidos</Link>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
