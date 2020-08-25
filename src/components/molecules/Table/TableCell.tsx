1f91f70e2bf90b49b3ca58948ff47ddb74efc948
Felipe Maccari <felipecesarmaccari@gmail.com>
2020-08-25 03:48:34 -0300

    refactor(aiq-design-system): refactor table

--- src/components/molecules/Table/TableCell.tsx
+++ src/components/molecules/Table/TableCell.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import PropTypes from 'prop-types'
 
 import styled from 'styled-components'
 
 import { Flex } from '../../atoms/Flex'
 
+export interface TableCellProps {
+  flex?: number
+  children: ReactNode
+}
+
 const TableCellStyled = styled(Flex)`
   padding: 0 10px;
   font-size: medium;
   justify-content: start;
-  flex: 1;
 `
 
-export const TableCell: React.FC = ({ children, ...props }) => {
-  return <TableCellStyled {...props}>{children}</TableCellStyled>
+export const TableCell: React.FC<TableCellProps> = ({
+  children,
+  flex,
+  ...props
+}) => {
+  return (
+    <TableCellStyled flex={flex} {...props}>
+      {children}
+    </TableCellStyled>
+  )
 }
 
 TableCell.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  flex: PropTypes.number
 }
--- src/components/molecules/Table/TableRow.tsx
+++ src/components/molecules/Table/TableRow.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import PropTypes from 'prop-types'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Flex } from '../../atoms/Flex'
 
-const TableRowStyled = styled(Flex)`
+export interface TableRowProps {
+  hoverable?: boolean
+  children: ReactNode
+}
+
+const TableRowStyled = styled(Flex)<TableRowProps>`
   justify-content: space-between;
   padding: 20px 0;
   cursor: pointer;
@@ -14,12 +19,29 @@ const TableRowStyled = styled(Flex)`
   &:last-child {
     border-bottom: none;
   }
+
+  ${({ hoverable, theme }) =>
+    hoverable &&
+    css`
+      &:hover {
+        background: ${theme.colors.lightGrey};
+      }
+    `}
 `
 
-export const TableRow: React.FC = ({ children, ...props }) => {
-  return <TableRowStyled {...props}>{children}</TableRowStyled>
+export const TableRow: React.FC<TableRowProps> = ({
+  children,
+  hoverable,
+  ...props
+}) => {
+  return (
+    <TableRowStyled hoverable={hoverable} {...props}>
+      {children}
+    </TableRowStyled>
+  )
 }
 
 TableRow.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  hoverable: PropTypes.bool
 }
