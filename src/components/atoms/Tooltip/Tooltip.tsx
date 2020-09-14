2b0cf575604a57eba1ecbd2603e7c50bbe4c8260
Juliano Sirtori <julianosirtori@gmail.com>
2020-09-14 16:06:51 -0300

    feat(tooltip) add type balloon

--- src/components/atoms/Tooltip/Tooltip.tsx
+++ src/components/atoms/Tooltip/Tooltip.tsx
@@ -7,14 +7,14 @@ import { Box, Props as BoxProps } from '../Box'
 export interface Props extends BoxProps {
   children?: ReactNode
   body?: any
-  variant?: 'top' | 'right' | 'bottom' | 'left' | 'right-bottom'
+  variant?: 'top' | 'right' | 'bottom' | 'left' | 'right-bottom' | 'left-bottom'
+  type?: 'default' | 'balloon'
 }
 
 const tooltipVariations = {
   top: css`
     bottom: 100%;
     left: 50%;
-    margin-left: -60px;
   `,
   right: css`
     top: -5px;
@@ -23,7 +23,6 @@ const tooltipVariations = {
   bottom: css`
     top: 100%;
     left: 50%;
-    margin-left: -60px;
   `,
   left: css`
     top: -5px;
@@ -32,6 +31,68 @@ const tooltipVariations = {
   'right-bottom': css`
     top: 100%;
     left: 80%;
+  `,
+  'left-bottom': css`
+    top: 100%;
+    right: 80%;
+  `
+}
+
+const typesVariants = {
+  default: css`
+    background-color: #fff;
+    color: ${({ theme }) => theme.colors.almostBlack};
+  `,
+  balloon: css<Props>`
+    color: #fff;
+    background-color: ${({ theme }) => theme.colors.almostBlack};
+
+    &::after {
+      content: '';
+      position: absolute;
+      border-width: 5px;
+      border-style: solid;
+    }
+
+    ${({ variant, theme }) => {
+      switch (variant) {
+        case 'top':
+          return css`
+            bottom: 136%;
+            left: 0;
+            &::after {
+              top: 100%;
+              left: 8px;
+              border-color: ${theme.colors.almostBlack} transparent transparent
+                transparent;
+            }
+          `
+        case 'bottom':
+          return css`
+            top: 136%;
+            left: 0;
+            &::after {
+              bottom: 100%;
+              left: 8px;
+
+              border-color: transparent transparent ${theme.colors.almostBlack}
+                transparent;
+            }
+          `
+        default:
+          return css`
+            top: 136%;
+            left: 0;
+            &::after {
+              bottom: 100%;
+              left: 8px;
+
+              border-color: transparent transparent ${theme.colors.almostBlack}
+                transparent;
+            }
+          `
+      }
+    }}
   `
 }
 
@@ -45,20 +106,24 @@ const TooltipStyled = styled(Box)<Props>`
 
   .tooltip {
     visibility: hidden;
+    opacity: 0;
     white-space: nowrap;
-    background-color: #fff;
-    color: ${({ theme }) => theme.colors.almostBlack};
     text-align: center;
     border-radius: 4px;
     position: absolute;
     z-index: 1;
     box-shadow: 0px 3px 6px #00000029;
+    transition: all 0.3s;
+    padding: 4px 8px;
 
     ${({ variant }) => tooltipVariations[variant || 'right']}
+
+    ${({ type }) => typesVariants[type || 'default']}
   }
 
   &:hover .tooltip {
     visibility: visible;
+    opacity: 1;
   }
 `
 
@@ -74,5 +139,14 @@ export const Tooltip: React.FC<Props> = ({ body, children, ...props }) => {
 
 Tooltip.propTypes = {
   body: PropTypes.any,
-  children: PropTypes.node
+  children: PropTypes.node,
+  type: PropTypes.oneOf(['default', 'balloon']),
+  variant: PropTypes.oneOf([
+    'top',
+    'right',
+    'bottom',
+    'left',
+    'right-bottom',
+    'left-bottom'
+  ])
 }
