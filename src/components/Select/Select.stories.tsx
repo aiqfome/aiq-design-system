02ff89a02276a6e0da29f4104dfda28e1f4ce662
Juliano Sirtori <julianosirtori@gmail.com>
2020-09-23 22:31:58 -0300

    feat(Select): add autocomplete prop   (#142)
    * open select item when click on input
    
    * feat(select): add prop autocomplete false
    
    * feat(select): alter type items
    
    * fix(Select): fix storie
    
    * fix(Select): test

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withKnobs, text, object } from '@storybook/addon-knobs'
 import { Select } from './Select'
+import { Flex } from '../Flex'
 
 export default {
   component: Select,
@@ -33,5 +34,35 @@ export const Outlined: React.FC = () => {
       label={text('label', 'aiq-design-system')}
       items={object('items', itens)}
     />
+  )
+}
+
+export const AutocompleteFalse: React.FC = () => {
+  const itens = [
+    { label: 'React', value: 1 },
+    { label: 'CSS', value: 2 },
+    { label: 'PHP', value: 3 },
+    { label: 'HTML', value: 4 }
+  ]
+  const [selectedItem, setSelectedItem] = useState(itens[0])
+  return (
+    <Flex variant='fullCentralized'>
+      <Select
+        mr='32px'
+        autoComplete={false}
+        variant='outlined'
+        label={text('label', 'aiq-design-system')}
+        items={object('items', itens)}
+      />
+      <Select
+        selectedItem={selectedItem}
+        autoComplete={false}
+        handleSelectedItemChange={({ selectedItem }) =>
+          setSelectedItem(selectedItem)
+        }
+        label={text('label', 'aiq-design-system')}
+        items={object('items', itens)}
+      />
+    </Flex>
   )
 }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -4,42 +4,43 @@ import PropTypes from 'prop-types'
 import { useCombobox } from 'downshift'
 import { IoIosArrowDown } from 'react-icons/io'
 
-import { Box } from '../Box'
+import { Box, Props as BoxPros } from '../Box'
 import { Input } from '../Input'
 import { Button, Props as ButtonProps } from '../Button'
 
-export interface Props {
+export interface Props extends BoxPros {
   label?: string
-  items?: string[]
+  items?: Array<string | { value: any; label: any }>
   isOpen?: boolean
   variant?: 'outlined'
   prefix?: any
   placeholder?: string
   handleSelectedItemChange?: (item: any) => void
   selectedItem?: any
-
-  backgroundColor?: any
-  border?: any
-  width?: any
-  maxWidth?: any
+  autoComplete?: boolean
 }
 
 const Container = styled(Box)<Props>`
   position: relative;
+
   ul {
+    box-shadow: 0px 3px 6px #00000029;
     background: ${({ theme }) => theme.colors.white};
     border: 1px solid ${({ theme }) => theme.colors.lightGrey};
-    padding: 10px;
     list-style-type: none;
     position: absolute;
     top: ${({ variant }) => (variant === 'outlined' ? '39px' : '38px')};
     overflow: hidden;
     z-index: 1;
     width: 100%;
+    padding: 0;
+    margin: 0;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
 
     li {
       cursor: pointer;
-      padding: 8px;
+      padding: 6px 12px;
     }
 
     ${({ isOpen }) =>
@@ -48,6 +49,14 @@ const Container = styled(Box)<Props>`
         display: none;
       `}
   }
+`
+interface ItemProps {
+  highlighted?: boolean
+}
+
+const Item = styled.li<ItemProps>`
+  background: ${({ highlighted, theme }) =>
+    highlighted ? theme.colors.primaryLight : '#fff'};
 `
 
 interface ButtonStyledProps extends ButtonProps {
@@ -67,7 +76,10 @@ export const Select: React.FC<Props> = ({
   items = [],
   placeholder,
   selectedItem,
-  handleSelectedItemChange,
+  autoComplete = true,
+  handleSelectedItemChange = () => {
+    // do nothing.
+  },
   prefix,
   ...props
 }) => {
@@ -88,19 +100,30 @@ export const Select: React.FC<Props> = ({
     getComboboxProps,
     highlightedIndex,
     getToggleButtonProps,
+    openMenu,
     getItemProps
   } = useCombobox({
+    onSelectedItemChange: handleSelectedItemChange,
     items: inputItems,
     selectedItem,
-    onSelectedItemChange: handleSelectedItemChange,
+    itemToString: item => (typeof item === 'string' ? item : item.label),
     onInputValueChange: ({ inputValue = '' }) => {
-      setInputItems(
-        items.filter(item =>
-          item.toLowerCase().startsWith(inputValue.toLowerCase())
+      if (autoComplete) {
+        setInputItems(
+          items.filter(item => {
+            const label = typeof item === 'string' ? item : item.label
+            return label.toLowerCase().startsWith(inputValue.toLowerCase())
+          })
         )
-      )
+      }
     }
   })
+
+  function handleClickInput() {
+    if (!isOpen) {
+      openMenu()
+    }
+  }
 
   return (
     <Container isOpen={isOpen} variant={variant} {...props}>
@@ -109,15 +132,13 @@ export const Select: React.FC<Props> = ({
           inputItems &&
           inputItems.length > 0 &&
           inputItems.map((item, index) => (
-            <li
-              style={
-                highlightedIndex === index ? { backgroundColor: '#bde4ff' } : {}
-              }
-              key={`${item}${index}`}
+            <Item
+              key={`${index}`}
+              highlighted={highlightedIndex === index}
               {...getItemProps({ item, index })}
             >
-              {item}
-            </li>
+              {typeof item === 'string' ? item : item.label}
+            </Item>
           ))}
       </ul>
 
@@ -126,10 +147,12 @@ export const Select: React.FC<Props> = ({
           onChange={getInputProps().onChange}
           onBlur={getInputProps().onBlur}
           onKeyDown={getInputProps().onKeyDown}
+          onClick={handleClickInput}
           value={getInputProps().value}
           inputRef={getInputProps().ref}
           variant={variant}
           label={label}
+          readOnly={!autoComplete}
           prefix={prefix}
           placeholder={placeholder}
           {...boxStyled}
@@ -139,8 +162,8 @@ export const Select: React.FC<Props> = ({
             palette='primary'
             mr={5}
             variantSelect={variant}
-            refButton={getToggleButtonProps().ref}
             onClick={getToggleButtonProps().onClick}
+            refButton={getToggleButtonProps().ref}
             aria-label='toggle menu'
           >
             <IoIosArrowDown />
@@ -160,7 +183,7 @@ Select.propTypes = {
   placeholder: PropTypes.string,
   handleSelectedItemChange: PropTypes.func,
   selectedItem: PropTypes.any,
-
+  autoComplete: PropTypes.bool,
   backgroundColor: PropTypes.any,
   border: PropTypes.any,
   width: PropTypes.any,
