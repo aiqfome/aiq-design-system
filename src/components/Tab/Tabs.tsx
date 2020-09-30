2d5c99331bcca420a52e973428e4916de934fd23
Vitor Bueno de Camargo <vitorcamargo@alunos.utfpr.edu.br>
2020-09-30 18:27:50 -0300

    feat(container-tabs-extra): fix responsivity

--- src/components/Tab/Tabs.tsx
+++ src/components/Tab/Tabs.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { SpaceProps, space, LayoutProps, layout } from 'styled-system'
 import styled, { css, DefaultTheme } from 'styled-components'
 
+import { Flex } from '../Flex'
+
 export interface TabsProps extends SpaceProps, LayoutProps, DefaultTheme {
-  value?: number
+  extra?: any
   children?: any
+  isMobile?: boolean
   variant?: 'default' | 'contained' | 'card'
+  scrollPosition?: 'left' | 'middle' | 'right'
   onChange?: (event: any, newValue: any) => void
 }
 
@@ -30,6 +34,8 @@ const TabStyled = styled.ul<TabsProps>`
   flex-direction: row;
   list-style: none;
   margin-bottom: 20px;
+  align-items: center;
+  width: 100%;
 
   ${space}
   ${layout}
@@ -37,26 +43,148 @@ const TabStyled = styled.ul<TabsProps>`
   ${({ variant }) => tabsVariations[variant || 'default']}
 `
 
+const FlexStyled = styled(Flex)<TabsProps>`
+  position: relative;
+  flex: 1;
+  overflow: hidden;
+
+  ::before {
+    position: absolute;
+    z-index: 1;
+    opacity: 0;
+    -webkit-transition: opacity 0.3s;
+    transition: opacity 0.3s;
+    content: '';
+    pointer-events: none;
+    left: 0;
+    -webkit-box-shadow: inset 10px 0 8px -8px rgba(0, 0, 0, 0.39);
+    box-shadow: inset 10px 0 8px -8px rgba(0, 0, 0, 0.39);
+    top: 0;
+    bottom: 0;
+    width: 30px;
+  }
+
+  ::after {
+    position: absolute;
+    z-index: 1;
+    opacity: 0;
+    -webkit-transition: opacity 0.3s;
+    transition: opacity 0.3s;
+    content: '';
+    pointer-events: none;
+    right: 0;
+    -webkit-box-shadow: inset -10px 0 8px -8px rgba(0, 0, 0, 0.39);
+    box-shadow: inset -10px 0 8px -8px rgba(0, 0, 0, 0.39);
+    top: 0;
+    bottom: 0;
+    width: 30px;
+  }
+
+  ${({ scrollPosition, isMobile }) => {
+    if (isMobile) {
+      if (scrollPosition === 'left') {
+        return css`
+          ::after {
+            opacity: 1;
+          }
+        `
+      }
+
+      if (scrollPosition === 'right') {
+        return css`
+          ::before {
+            opacity: 1;
+          }
+        `
+      }
+
+      return css`
+        ::after {
+          opacity: 1;
+        }
+
+        ::before {
+          opacity: 1;
+        }
+      `
+    }
+  }}
+`
+
 export const Tabs: React.FC<TabsProps> = ({
+  extra,
   children,
   onChange = () => {
     // do nothing.
   },
   ...props
 }) => {
+  const [isMobile, setIsMobile] = useState(false)
+  const [ref, setRef] = useState<HTMLDivElement | null>(null)
+  const [scrollPosition, setScrollPosition] = useState<
+    'left' | 'middle' | 'right'
+  >('left')
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref && ref.scrollWidth !== ref.clientWidth) {
+        setIsMobile(true)
+      } else {
+        setIsMobile(false)
+      }
+    }
+
+    if (ref) {
+      handleResize()
+      window.addEventListener('resize', handleResize)
+    }
+  }, [ref])
+
+  useEffect(() => {
+    const handleScroll = e => {
+      if (ref && isMobile) {
+        ref.scrollLeft += e.deltaY
+
+        if (ref.scrollLeft === 0) {
+          setScrollPosition('left')
+        } else if (ref.offsetWidth + ref.scrollLeft === ref.scrollWidth) {
+          setScrollPosition('right')
+        } else {
+          setScrollPosition('middle')
+        }
+      }
+    }
+
+    if (ref) {
+      ref.addEventListener('wheel', handleScroll)
+    }
+  }, [ref, isMobile])
+
   function handleClick(event) {
     onChange(event, parseInt(event.currentTarget.dataset.id))
   }
 
   return (
-    <TabStyled onClick={handleClick} {...props}>
-      {children}
+    <TabStyled {...props}>
+      <FlexStyled isMobile={isMobile} scrollPosition={scrollPosition}>
+        <Flex
+          flex={1}
+          overflow='hidden'
+          position='relative'
+          onClick={handleClick}
+          ref={el => setRef(el || null)}
+        >
+          {children}
+        </Flex>
+      </FlexStyled>
+
+      <Flex px='12px'>{extra}</Flex>
     </TabStyled>
   )
 }
 
 Tabs.propTypes = {
-  value: PropTypes.number,
+  extra: PropTypes.any,
   children: PropTypes.any,
   onChange: PropTypes.func,
   variant: PropTypes.oneOf(['default', 'contained', 'card'])
