7a1fb3a5f80c26223af14c7dcb8ea0a861fa8a7c
Juliano Sirtori <julianosirtori@gmail.com>
2020-10-21 11:18:30 -0300

    feat(button): finish tests button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -49,18 +49,56 @@ describe('Button', () => {
     expect(handleOnClick).not.toHaveBeenCalled()
   })
 
-  it('should not have to call onClick when click on button disabled', () => {
-    const handleOnClick = jest.fn()
+  it('should have classes __button-text and __button-text-primary when variant and palette is null', () => {
+    const { getByRole } = render(<Button>Test</Button>)
 
+    expect(getByRole('button')).toHaveClass('__button-text')
+    expect(getByRole('button')).toHaveClass('__button-text-primary')
+  })
+
+  it('should have class __button-text when variant equal text', () => {
     const { getByRole } = render(
-      <Button disabled={true} onClick={handleOnClick}>
+      <Button variant='text' palette='primary'>
         Test
       </Button>
     )
+
+    expect(getByRole('button')).toHaveClass('__button-text')
+  })
+
+  it('should have class __button-contained when variant equal contained', () => {
+    const { getByRole } = render(<Button variant='contained'>Test</Button>)
+
     const button = getByRole('button')
 
-    fireEvent.click(button)
+    expect(button).toHaveClass('__button-contained')
+    expect(button).toHaveClass('__button-contained-primary')
+  })
 
-    expect(handleOnClick).not.toHaveBeenCalled()
+  it('should have class __button-outlined when variant equal outlined', () => {
+    const { getByRole } = render(<Button variant='outlined'>Test</Button>)
+
+    const button = getByRole('button')
+
+    expect(button).toHaveClass('__button-outlined')
+    expect(button).toHaveClass('__button-outlined-primary')
+  })
+
+  it('should have class __button-fab when variant equal fab', () => {
+    const { getByRole } = render(<Button variant='fab'>Test</Button>)
+
+    const button = getByRole('button')
+
+    expect(button).toHaveClass('__button-fab')
+    expect(button).toHaveClass('__button-fab-primary')
+  })
+
+  it('should have class __button-icon when variant equal icon', () => {
+    const { getByRole } = render(<Button variant='icon'>Test</Button>)
+
+    const button = getByRole('button')
+
+    expect(button).toHaveClass('__button-icon')
+    expect(button).toHaveClass('__button-icon-primary')
   })
 })
