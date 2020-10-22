d68f63586ecca6d270f1b4a2cb9e06453ac6aa28
Juliano Sirtori <julianosirtori@gmail.com>
2020-10-22 16:13:15 -0300

    feat(breadcrumb): add test (#205)

--- src/components/utils/test/render.tsx
+++ src/components/utils/test/render.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+
 import { render } from '@testing-library/react'
+
+import { BrowserRouter } from 'react-router-dom'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
 
-const wrapper = ({ children, ...props }) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+const wrapper = ({ children }) => {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </BrowserRouter>
+  )
 }
 
 const customRender = (ui, options?: any) => render(ui, { wrapper, ...options })
