a712b7607a6a2ce68cdb252e497eaa83dac66cee
Vitor Bueno de Camargo <vitorcamargo@alunos.utfpr.edu.br>
2020-10-29 03:46:31 -0300

    refactor(drawer): fix drawer problems

--- src/components/Drawer/Drawer.stories.tsx
+++ src/components/Drawer/Drawer.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs'
 
 import { Text } from '../Text'
 import { Drawer } from './Drawer'
+import { Button } from '../Button'
 
 export default {
   component: Drawer,
@@ -18,6 +19,48 @@ export const basic: React.FC = () => (
     <Text>{text('text', 'Design System')}</Text>
   </Drawer>
 )
+
+export const WithMask: React.FC = () => {
+  const [opened, setOpened] = useState(false)
+
+  return (
+    <>
+      <Button onClick={() => setOpened(true)}>clique aqui para abrir</Button>
+      <Drawer opened={opened} onClose={() => setOpened(false)}>
+        <Text>Design System</Text>
+      </Drawer>
+    </>
+  )
+}
+
+export const WithLoading: React.FC = () => {
+  const [opened, setOpened] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const openDrawer = () => {
+    setOpened(true)
+    setTimeout(() => setLoading(false), 2000)
+  }
+
+  const closeDrawer = () => {
+    setOpened(false)
+    setLoading(true)
+  }
+
+  return (
+    <>
+      <Button onClick={openDrawer}>clique aqui para abrir</Button>
+      <Drawer
+        width='50%'
+        opened={opened}
+        loading={loading}
+        onClose={closeDrawer}
+      >
+        <Text>Design System</Text>
+      </Drawer>
+    </>
+  )
+}
 
 export const left: React.FC = () => (
   <Drawer opened={true} variation='left'>
