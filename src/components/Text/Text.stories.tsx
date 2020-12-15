c5310c889bc7be3671c4e0561dab3b0b1a75851d
Felipe Maccari <felipecesarmaccari@gmail.com>
2020-12-15 15:16:15 -0300

    feat(text): update stories

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { withKnobs, text, number } from '@storybook/addon-knobs'
+import { withKnobs, text, number, boolean } from '@storybook/addon-knobs'
 
 import { Text } from './Text'
 import { Flex } from '../Flex'
@@ -10,23 +10,32 @@ export default {
   decorators: [withKnobs]
 }
 
-export const basic: React.FC = () => (
-  <Text
-    color={text('color', 'primary')}
-    fontSize={number('fontSize', 16)}
-    cursor={text('cursor', '')}
-    fontWeight={text('fontWeight', 'medium')}
-  >
-    {text('text', 'Aiqfome')}
-  </Text>
+export const Basic: React.FC = () => (
+  <Flex variant='fullCentralized'>
+    <Text
+      color={text('color', 'primary')}
+      fontSize={number('fontSize', 22)}
+      width={text('width', '100%')}
+      cursor={text('cursor', '')}
+      fontWeight={text('fontWeight', 'semiBold')}
+      truncate={boolean('truncate', false)}
+      textAlign='center'
+    >
+      {text('text', 'o design system do app mais fominha da internê!')}
+    </Text>
+  </Flex>
 )
 
-export const cursor: React.FC = () => (
-  <Text cursor='pointer'>Design System</Text>
-)
-
-export const WhiteSpace: React.FC = () => (
-  <Flex width='50px' border='1px solid black'>
-    <Text whiteSpace='nowrap'>Design System</Text>
+export const Truncated: React.FC = () => (
+  <Flex variant='fullCentralized'>
+    <Text
+      color='primary'
+      fontWeight='700'
+      width='180px'
+      whiteSpace='nowrap'
+      truncate
+    >
+      o texto truncado fica assim!
+    </Text>
   </Flex>
 )
