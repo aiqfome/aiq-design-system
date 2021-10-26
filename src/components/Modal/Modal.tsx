d7c2f17a26fabf7833265e3b5af6f578b16c8fb8
Fernando <fernandoborgesjr@outlook.com>
2021-10-26 11:29:10 -0300

    fix(Modal, Alert): fix error getting class name

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -133,7 +133,7 @@ const ModalStyled = styled(Flex)<ModalStyledProps>`
 
   &.show {
     opacity: 1;
-    z-index: 2000;
+    z-index: 2010;
   }
 
   ${({ animation }) =>
@@ -180,7 +180,7 @@ export const Modal: React.FC<Props> = ({
   title,
   children,
   show = false,
-  zIndex = 2000,
+  zIndex = 2010,
   animation = false,
   formProps,
   buttonsProps,
@@ -232,7 +232,10 @@ export const Modal: React.FC<Props> = ({
   }
 
   function handleClickOutSide({ target }) {
-    if (target.className && target.className.includes('background-modal')) {
+    if (
+      typeof target?.className === 'string' &&
+      target.className.includes('background-modal')
+    ) {
       onClose()
     }
   }
