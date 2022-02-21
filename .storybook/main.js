347eba54148966407469153e5fb8bdc290f89896
Felipe Cesar Maccari <felipecesarmaccari@gmail.com>
2022-02-21 11:09:33 -0300

    feat(storybook): fix chromatic build (#391)
    Co-authored-by: Felipe Maccari <felipemaccari@aiqfome.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,29 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-docs', '@storybook/addon-knobs/register'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-docs'
+  ],
   webpackFinal: async config => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       include: path.resolve(__dirname, '..'),
       use: [
         {
-          loader: require.resolve('ts-loader'),
+          loader: require.resolve('ts-loader')
         },
         {
           loader: require.resolve('react-docgen-typescript-loader'),
           options: {
-            tsconfigPath: path.resolve(__dirname, '..', 'tsconfig.json'),
-          },
-        },
-      ],
-    });
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
-};
+            tsconfigPath: path.resolve(__dirname, '..', 'tsconfig.json')
+          }
+        }
+      ]
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  }
+}
