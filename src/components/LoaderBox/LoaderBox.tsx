42189d9f784be6b714dec83d37cc2e45991c0449
Camille Gachido <camillegachido70@gmail.com>
2023-01-09 15:46:45 -0300

    feat(loaderBox):  add args

--- src/components/LoaderBox/LoaderBox.tsx
+++ src/components/LoaderBox/LoaderBox.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { Flex } from '../Flex'
+import { Flex, Props } from '../Flex'
 import { Loading } from '../Loading'
 
-export const LoaderBox: React.FC = ({ ...props }) => (
+export const LoaderBox: React.FC = ({ ...props }: Props) => (
   <Flex justifyContent='center' alignItems='center' height='100vh' {...props}>
     <Loading />
   </Flex>
